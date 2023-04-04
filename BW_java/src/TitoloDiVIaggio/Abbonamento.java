7aa62f5a38766efa29c197dab7f5b87638f4fbdc
IlBennys <119436512+IlBennys@users.noreply.github.com>
2023-04-04 16:31:54 +0200

    OK

--- BW_java/src/TitoloDiVIaggio/Abbonamento.java
+++ BW_java/src/TitoloDiVIaggio/Abbonamento.java
@@ -1,5 +1,7 @@
 package TitoloDiVIaggio;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.InputMismatchException;
 
 import javax.persistence.DiscriminatorValue;
@@ -10,64 +12,79 @@ import javax.persistence.Table;
 
 import Enum.Periodicità;
 import Main.GestioneTrasporti;
+import lombok.Getter;
 
 @Entity
 @Table(name = "abbonamenti")
 @DiscriminatorValue("Abbonamento")
 public class Abbonamento extends TitoloDiVIaggio {
 
-	@Enumerated(EnumType.STRING)
-	private Periodicità periodicità;
-	private Boolean validità;
+    @Enumerated(EnumType.STRING)
+    private Periodicità periodicità;
+    private Boolean validità;
+    private @Getter LocalDate scadenzaAbbonamento;
 
-	//Costruttori
-	
-	public Abbonamento () {}
-	
-	public Abbonamento (Boolean validità) {
-		super();
-		this.validità = validità;
-		this.setPeriodicità();
-	}
-	
-	public Abbonamento(Periodicità periodicità, Boolean validità) {
-		super();
-		this.periodicità = periodicità;
-		this.validità = validità;
-	}
+    // Costruttori
 
-	//Getter and Setter
-	
-	public Periodicità getPeriodicità() {
-		return periodicità;
-	}
+    public Abbonamento() {
+    }
 
-	public void setPeriodicità() {
-		int uscita = 0;
-		while(uscita <= 0 || uscita >= 4) {
-			System.out.println(">> Seleziona la durata dell'abbonamento 1 = Settimanale 2 = Mensile");
-			try {
-				  uscita = GestioneTrasporti.s.nextInt();
-				  GestioneTrasporti.s.nextLine();
-				switch(uscita){
-				case 1:
-					this.periodicità = Periodicità.SETTIMANALE;
-					break;
-				case 2:
-					this.periodicità = Periodicità.MENSILE;
-					break;
-				}
-			}catch(InputMismatchException e) {
-				System.out.println("Utilizza un valore valido");
-				GestioneTrasporti.s.nextInt();
-			}
-			
+    public Abbonamento(Boolean validità) {
+	super();
+	this.validità = validità;
+	this.setPeriodicità();
+    }
+
+    public Abbonamento(Periodicità periodicità, Boolean validità) {
+	super();
+	this.periodicità = periodicità;
+	this.validità = validità;
+    }
+
+    // Getter and Setter
+
+    public Periodicità getPeriodicità() {
+	return periodicità;
+    }
+
+    public void setPeriodicità() {
+	int uscita = 0;
+	while (uscita <= 0 || uscita >= 4) {
+	    System.out.println(">> Seleziona la durata dell'abbonamento 1 = Settimanale 2 = Mensile");
+	    try {
+		uscita = GestioneTrasporti.s.nextInt();
+		GestioneTrasporti.s.nextLine();
+		switch (uscita) {
+		case 1:
+		    this.periodicità = Periodicità.SETTIMANALE;
+		    break;
+		case 2:
+		    this.periodicità = Periodicità.MENSILE;
+		    break;
 		}
+	    } catch (InputMismatchException e) {
+		System.out.println("Utilizza un valore valido");
+		GestioneTrasporti.s.nextInt();
+	    }
+
+	}
+    }
+
+    public Boolean getValidità() {
+	return validità;
+    }
+
+    public void setScadenzaAbbonamento() {
+	LocalDate d;
+
+	d = this.dataemissione.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+	if (getPeriodicità().equals(Periodicità.SETTIMANALE)) {
+	    this.scadenzaAbbonamento = d.plusWeeks(1);
+	} else {
+	    this.scadenzaAbbonamento = d.plusMonths(1);
 	}
 
-	public Boolean getValidità() {
-		return validità;
-	}
-	
-	
+    }
+
 }
--- BW_java/src/TitoloDiVIaggio/TitoloDiVIaggio.java
+++ BW_java/src/TitoloDiVIaggio/TitoloDiVIaggio.java
@@ -6,136 +6,141 @@ import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Random;
 
-import javax.persistence.*;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 import Enum.Ticket_Office;
 import Main.GestioneTrasporti;
 
-
 @Entity
-@Table(name= "titoli_di_viaggio")
+@Table(name = "titoli_di_viaggio")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "Articolo", discriminatorType = DiscriminatorType.STRING)
 @NamedQuery(name = "Elementi.FindAll", query = "SELECT t FROM TitoloDiVIaggio t")
 public class TitoloDiVIaggio {
 
-	@Id
+    @Id
     private Long id;
-	
-	@Enumerated(EnumType.STRING)
+
+    @Enumerated(EnumType.STRING)
     private Ticket_Office puntoemissione;
 
-    private Date dataemissione;
-    
- 
-    
+    protected Date dataemissione;
 
-    
-	public TitoloDiVIaggio() {
-		
-		//this.setId();
-		//this.setPuntoemissione();
-		//this.setDataemissione();
-	}		
+    public TitoloDiVIaggio() {
 
-	public TitoloDiVIaggio(Long id, Ticket_Office puntoemissione, Date dataemissione) {
-		super();
-		this.id = id;
-		this.puntoemissione = puntoemissione;
-		this.dataemissione = dataemissione;
+	// this.setId();
+	// this.setPuntoemissione();
+	// this.setDataemissione();
+    }
+
+    public TitoloDiVIaggio(Long id, Ticket_Office puntoemissione, Date dataemissione) {
+	super();
+	this.id = id;
+	this.puntoemissione = puntoemissione;
+	this.dataemissione = dataemissione;
+    }
+
+    // Setter and Getter
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId() {
+
+	Long num = System.currentTimeMillis();
+	Random rnum = new Random(num);
+	Long isbn = Math.abs(rnum.nextLong()) % 9000000000000L + 1000000000000L;
+	isbn *= 10L;
+	this.id = isbn;
+
+    }
+
+    public Ticket_Office getPuntoemissione() {
+	return puntoemissione;
+    }
+
+    public void setPuntoemissione(int o) {
+	// int uscita = 0;
+	// while(uscita <= 0 || uscita >= 4) {
+	// System.out.println(">> Seleziona il punto di acquisto 1 = Distributore
+	// automatico 2 = Rivenditore autorizzato");
+	try {
+	    // uscita = GestioneTrasporti.s.nextInt();
+	    int uscita = o;
+	    switch (uscita) {
+	    case 1:
+		this.puntoemissione = Ticket_Office.DISTRIBUTORE_AUTOMATICO;
+		break;
+	    case 2:
+		this.puntoemissione = Ticket_Office.RIVENDITORE_AUTORIZZATO;
+		break;
+	    }
+	} catch (InputMismatchException e) {
+	    System.out.println("Error " + e);
 	}
 
-	//Setter and Getter
-	
-	public Long getId() {
-		return id;
-	}
+	// }
+    }
 
-	public void setId() {
-		
-		Long num = System.currentTimeMillis();
-		Random rnum = new Random(num);
-		Long isbn = Math.abs(rnum.nextLong()) % 9000000000000L + 1000000000000L;
-		isbn *= 10L;
-		this.id = isbn;
-		
-	}
+    public Date getDataemissione() {
+	return dataemissione;
+    }
 
-	public Ticket_Office getPuntoemissione() {
-		return puntoemissione;
-	}
+    public void setDataemissione() {
+	System.out.println(">> inserisci la data di emissione (DD/MM/YYYY)");
+	String s = GestioneTrasporti.s.nextLine();
 
-	public void setPuntoemissione(int o) {
-		//int uscita = 0;
-		//while(uscita <= 0 || uscita >= 4) {
-			//System.out.println(">> Seleziona il punto di acquisto 1 = Distributore automatico 2 = Rivenditore autorizzato");
-			try {
-				//  uscita = GestioneTrasporti.s.nextInt();
-				int uscita = o;
-				switch(uscita){
-				case 1:
-					this.puntoemissione = Ticket_Office.DISTRIBUTORE_AUTOMATICO;
-					break;
-				case 2:
-					this.puntoemissione = Ticket_Office.RIVENDITORE_AUTORIZZATO;
-					break;
-				}
-			}catch(InputMismatchException e) {
-				System.out.println("Error " + e);
-			}
-			
-		//}
-	}
-
-	public Date getDataemissione() {
-		return dataemissione;
-	}
-
-	public void setDataemissione() {
-		System.out.println(">> inserisci la data di emissione (DD/MM/YYYY)");
-		String s = GestioneTrasporti.s.nextLine();
-		
-		while(true) {
-			try {
-				SimpleDateFormat formatdate = new SimpleDateFormat("dd/MM/yyyy");
-				Calendar today = Calendar.getInstance();
-				Date oggi =(Date) today.getTime();
-				Date date = new Date(formatdate.parse(s).getTime());
-				if(date.after(oggi)) {
-					System.err.println("Non sapevo sapessi viaggiare nel tempo! Prova un data valida");
-					s = GestioneTrasporti.s.nextLine();
-				} else {				
-					this.dataemissione = date;
-					break;
-				}
-			} catch (Exception e) {
-				System.out.println("Inserisci una data valida!");
-				s = GestioneTrasporti.s.nextLine();
-			}
+	while (true) {
+	    try {
+		SimpleDateFormat formatdate = new SimpleDateFormat("dd/MM/yyyy");
+		Calendar today = Calendar.getInstance();
+		Date oggi = (Date) today.getTime();
+		Date date = new Date(formatdate.parse(s).getTime());
+		if (date.after(oggi)) {
+		    System.err.println("Non sapevo sapessi viaggiare nel tempo! Prova un data valida");
+		    s = GestioneTrasporti.s.nextLine();
+		} else {
+		    this.dataemissione = date;
+		    break;
 		}
-	
+	    } catch (Exception e) {
+		System.out.println("Inserisci una data valida!");
+		s = GestioneTrasporti.s.nextLine();
+	    }
 	}
-	
-	public static Date insertdata() {
-		
-		String s = GestioneTrasporti.s.nextLine();
-		
-		while(true) {
-			try {
-				SimpleDateFormat formatdate = new SimpleDateFormat("dd/MM/yyyy");
-				Date oggi =new Date();
-				Date date = new Date(formatdate.parse(s).getTime());
-				if(date.after(oggi)) {
-					System.err.println("La data per la verifica deve essere massimo oggi");
-					s = GestioneTrasporti.s.nextLine();
-				} else {				
-				return	date;
-				}
-			} catch (Exception e) {
-				System.out.println("Inserisci una data valida!");
-				s = GestioneTrasporti.s.nextLine();
-			}
+
+    }
+
+    public static Date insertdata() {
+
+	String s = GestioneTrasporti.s.nextLine();
+
+	while (true) {
+	    try {
+		SimpleDateFormat formatdate = new SimpleDateFormat("dd/MM/yyyy");
+		Date oggi = new Date();
+		Date date = new Date(formatdate.parse(s).getTime());
+		if (date.after(oggi)) {
+		    System.err.println("La data per la verifica deve essere massimo oggi");
+		    s = GestioneTrasporti.s.nextLine();
+		} else {
+		    return date;
 		}
+	    } catch (Exception e) {
+		System.out.println("Inserisci una data valida!");
+		s = GestioneTrasporti.s.nextLine();
+	    }
 	}
+    }
+
 }
-
